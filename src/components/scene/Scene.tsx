a93831463c840b034b0df489c2daba3b3b0d76bf
hirokisekiguchi-art <hiroki.sekiguchi@intimatemerger.com>
2026-06-18 17:03:11 +0900

    Update Scene.tsx

--- src/components/scene/Scene.tsx
+++ src/components/scene/Scene.tsx
@@ -1,32 +1,3 @@
-import { colors } from '../../data/colors';
-import { Lights } from './Lights';
-import { Sky } from './Sky';
-import { SlopeRoad } from '../world/SlopeRoad';
-import { Sidewalks } from '../world/Sidewalk';
-import { TownBlocks } from '../world/TownBlocks';
-import { StreetProps } from '../world/StreetProps';
-import { FarVehicle } from '../world/FarVehicle';
-import { Player } from '../player/Player';
-
-interface SceneProps {
-  started: boolean;
-}
-
-export function Scene({ started }: SceneProps) {
-  return (
-    <>
-      <color attach="background" args={[colors.sky]} />
-      <fog attach="fog" args={[colors.sky, 38, 84]} />
-      <Lights />
-      <Sky />
-      <group>
-        <SlopeRoad />
-        <Sidewalks />
-        <TownBlocks />
-        <StreetProps />
-        <FarVehicle />
-      </group>
-      <Player started={started} />
-    </>
-  );
+export function Scene() {
+  return null;
 }
